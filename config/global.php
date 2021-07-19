92a0091a2dad4337daedfb3b0e7a27cf899b6e17
robert1899 <rxrc1899@gmail.com>
2021-07-19 15:11:35 -0500

    se agrega el proyecto de sistema vacunacion

---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php
+
+//Ip de pC servidor Localhost
+define("DB_HOST","localhost");
+
+//Nombre de la base de datos
+define("DB_NAME","dbsistema");
+
+//Usuario base de datos
+define("DB_USERNAME","root");
+
+//Password  del usuario de la Base Datos
+define("DB_PASS","");
+
+//Codificacion de caracteres
+define("DB_ENCODE","utf-8");
+
+//Nnombre del proyecto
+define("PRO_NOMBRE","RiveraVentas");
+
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+//Direcionamiento a la vista Login
+
+header('Location:vista/login.html');
+?>
